9bd71311f55f6dfb813956306ba38d995deaa895
Adrien Chinour <git@chinour.fr>
2023-10-22 14:12:49 +0200

    fix: don't load WebProfiler config on prod

--- config/packages/web_profiler.php
+++ config/packages/web_profiler.php
@@ -3,6 +3,10 @@
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Config\WebProfilerConfig;
 
+if (!class_exists('\Symfony\Bundle\WebProfilerBundle\WebProfilerBundle')) {
+    return;
+}
+
 return static function (WebProfilerConfig $profiler, ContainerConfigurator $container) {
     if ($container->env() === 'dev') {
         $profiler
