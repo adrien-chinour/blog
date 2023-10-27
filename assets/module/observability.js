ae762334acfa57694ced4fd6cb10b34ea5b91b52
Adrien Chinour <git@chinour.fr>
2023-10-27 23:48:38 +0200

    chore: activate observability only on prod

--- assets/module/observability.js
+++ assets/module/observability.js
@@ -1,15 +1,17 @@
 import {getWebInstrumentations, initializeFaro} from '@grafana/faro-web-sdk';
 import {TracingInstrumentation} from '@grafana/faro-web-tracing';
 
-initializeFaro({
-  url: 'https://faro-collector-prod-eu-west-0.grafana.net/collect/9689c3ba5a20d52b36dec6a5da24f8eb',
-  app: {
-    name: 'udfn.fr',
-    version: '1.0.0',
-    environment: process.env.NODE_ENV
-  },
-  instrumentations: [
-    ...getWebInstrumentations(),
-    new TracingInstrumentation(),
-  ],
-});
+if (process.env.NODE_ENV === 'production') {
+  initializeFaro({
+    url: 'https://faro-collector-prod-eu-west-0.grafana.net/collect/9689c3ba5a20d52b36dec6a5da24f8eb',
+    app: {
+      name: 'udfn.fr',
+      version: '1.0.0',
+      environment: 'production'
+    },
+    instrumentations: [
+      ...getWebInstrumentations(),
+      new TracingInstrumentation(),
+    ],
+  });
+}
