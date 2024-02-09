1b8e8960e2df8a0f0a87dc088349965e06ef8299
Adrien Chinour <github@chinour.fr>
2024-02-09 23:34:14 +0100

    feat(seo): add rss feed

--- config/packages/twig.php
+++ config/packages/twig.php
@@ -6,6 +6,12 @@ use Symfony\Config\TwigConfig;
 return static function (TwigConfig $twig, ContainerConfigurator $container): void {
     $twig->defaultPath('%kernel.project_dir%/templates');
 
+    $twig->global('site')->value([
+        'name' => 'Undefined',
+        'description' => 'Un blog centré sur le développement web en PHP. Retrouvez des articles sur mes trouvailles et expérimentations.',
+        'url' => 'https://www.udfn.fr',
+    ]);
+
     if (in_array($container->env(), ['dev', 'test'], true)) {
         $twig->strictVariables(true);
     }
