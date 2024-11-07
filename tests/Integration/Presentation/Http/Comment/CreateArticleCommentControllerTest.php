a969265d6a4e7e2d8082e350d6e48bcb0c57dd8d
Adrien Chinour <github@chinour.fr>
2024-11-08 00:26:28 +0100

    test: add test on post comment

--- tests/Integration/Presentation/Http/Comment/CreateArticleCommentControllerTest.php
+++ tests/Integration/Presentation/Http/Comment/CreateArticleCommentControllerTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Tests\Integration\Presentation\Http\Comment;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
 
 final class CreateArticleCommentControllerTest extends WebTestCase
 {
@@ -14,5 +15,23 @@ final class CreateArticleCommentControllerTest extends WebTestCase
 
         $client->request('POST', '/comments');
         $this->assertResponseIsUnprocessable();
+    }
+
+    public function testCreateArticleCommentWithCorrectPayloadWillCreateComment(): void
+    {
+        $client = static::createClient();
+
+        $client->request(
+            'POST',
+            '/comments',
+            parameters: [
+                'articleId' => '1',
+                'username' => 'adrien',
+                'message' => 'ok',
+            ],
+        );
+
+        $this->assertResponseIsSuccessful();
+        $this->assertResponseStatusCodeSame(Response::HTTP_CREATED);
     }
 }
